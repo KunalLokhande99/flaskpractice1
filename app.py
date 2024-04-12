0242bb6be167e2fae1f1ab83b173ccdcba3ef9f8
Kunal Lokhande <kunallokhandea99@gmail.com>
2024-04-12 18:10:10 +0530

    Flask practice using postman

---
+++ app.py
@@ -0,0 +1,44 @@
+# simple app using flask and postman
+
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return 'Welcome to another flask tutorial'
+
+@app.route('/calculate', methods = ["GET"])
+def math_operator():
+    operation = request.json['operation']
+    number1 = request.json['number1']
+    number2 = request.json['number2']
+
+    if operation == 'add':
+        result = int(number1) + int(number2)
+    elif operation == 'subtract':
+        result = int(number1) - int(number2)
+    elif operation == 'multiply':
+        result = int(number1) * int(number2)
+    else:
+        result = int(number1) / int(number2)
+    #return jsonify(result)
+    return 'The operation is {} and Result is {}'.format(operation, result)
+
+
+print(__name__)
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
+# json code required for postman
+#{
+#"operation" : "multiply",
+#"number1" : 100,
+#"number2" : 20
+#}
